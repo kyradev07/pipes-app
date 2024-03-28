34fc92665cc0422b3eaf58f7ec8eb05fb75209ec
Kyra <kyra.dev.07@gmail.com>
2024-03-28 00:24:57 -0500

    Se ejemplifican pipes como json, keyvalue, async, slice, i18nPlural

--- src/app/products/pages/no-common/no-common.component.ts
+++ src/app/products/pages/no-common/no-common.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { delay, filter, interval, Observable, take, tap } from "rxjs";
 
 type Gender = 'male' | 'female';
 type Title = 'Mr.' | 'Mrs.';
@@ -8,6 +9,11 @@ type GenderMap = {
 type OptionMessage = '=0' | '=1' | 'other';
 type ClientMap = {
   [O in OptionMessage]: string
+}
+type Person = {
+  name: string;
+  age: number;
+  address: string;
 }
 
 @Component({
@@ -24,12 +30,37 @@ export class NoCommonComponent {
     'female': 'Mrs.',
   }
 
+  //i18nPlural
   clients: string[] = ['Marie', 'Nate', 'Sandra', 'Frederick', 'Jesus', 'Magdalena', 'Lena'];
   clientMap: ClientMap = {
     '=0': 'All clients were dispatched.',
     '=1': '1 client is waiting.',
     'other': '# clients are waiting.'
   }
+
+  //slice
+  customers: string[] = [...this.clients];
+
+  //json, keyValue
+  person: Person = {
+    name: 'Juan',
+    age: 78,
+    address: 'Medellín, Antioquia, Colombia'
+  }
+
+  //async
+  myObservable: Observable<number> = interval(500).pipe(
+    delay(5000),
+    filter((value: number): boolean => value > 0),
+    tap((value: number) => console.log(value)),
+    take(10)
+  );
+
+  myPromise: Promise<string> = new Promise((resolve, _): void => {
+    setTimeout((): void => {
+      resolve('There is data...')
+    }, 5000);
+  })
 
   changeGuest(): void {
     this.name = this.name === 'Sam' ? 'Elizabeth' : 'Sam';
@@ -39,4 +70,9 @@ export class NoCommonComponent {
   dispatchClient(): void {
     this.clients.shift();
   }
+
+  resetClients(): void {
+    this.clients = ['Marie', 'Nate', 'Sandra', 'Frederick', 'Jesus', 'Magdalena', 'Lena'];
+  }
+
 }
